3f78f2739f2bbc28b3d424ba827b7301ca078c96
Batista <140225853+Batissta@users.noreply.github.com>
2023-08-24 11:06:52 -0300

    Update comercio.java

--- classes/comercio.java
+++ classes/comercio.java
@@ -1,8 +1,24 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Comercio {
 	private Itens produtos;
 	
+	public void menu() {
+		System.out.println("Seja bem-vindo à nossa concessionária.\nFaça sua escolha:\n"
+				+ "1) Listar carros cadastrados.\n"
+				+ "2) Cadastrar novo carro.\n"
+				+ "3) Adicionar estoque a um carro existente.\n"
+				+ "4) Remover um produto do comércio.\n"
+				+ "5) Vender algum produto existente.\n"
+				+ "6) Sair");
+		Scanner t = new Scanner(System.in);
+		int esc = Integer.parseInt(t.nextLine());
+		if (esc == 2) {
+			cadastrar();
+		}
+	}
 	public boolean codigoExiste(long cod) {
 		Item selecionado = new Item(null);
 		for (Item item : produtos.getProdutos()) {
@@ -17,10 +33,12 @@ public class Comercio {
 		produtos.getProdutos().size();
 	}
 	public void adicionar() {
-		produtos.adicionar(23399, 0);
+		
 	}
 	public void remover() {
 		
 	}
-	
+	public void cadastrar() {
+		
+	}
 }
