d1ea245d4fced4f64d7a659a2e8ad80354968b89
gabryell <g.lealrocha3@gmail.com>
2023-08-24 15:55:15 -0300

    Try/catch corrigido

--- classes/comercio.java
+++ classes/comercio.java
@@ -3,17 +3,38 @@ package classes;
 //Alteracoes de Gabryell leal:	1) edicao em listar(): adicionei um if/else para tratar a listagem dependendo 
 //								   de quantos itens existem na lista.
 //								2) tratamento de erros do menu: Para evitar que o codigo quebre em diferentes situacoes, criei um Try/catch onde: Se o usuario digitar algo invalido, 
-//								   uma mensagem de erro sera exibida. Ainda falta ajustes nesse tratamento. DO jeito que esta, o codigo vai parar assim que aparecer a mensagem, 
-//								   preciso ver uma forma de fazer com que volte para a escolha do menu.
+//								   uma mensagem de erro sera exibida. O try deixa o codigo rolar ate que um erro de digitacao ou utilizacao de caracteres que nao queremos seja utilizadas. 
+//								   dai entra o tratamento do processo com o SWITCH.
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+
 public class comercio {
-	private itens produtos;
-	
-	public void menu() {
-		try
-		{
+
+private itens produtos;
+{
+	Scanner t = new Scanner(System.in);
+	int esc = 0;
+	while (esc != 6) 
+	{
+		menu(); //mostra o menu
+		try{	//percorre a escolha do usuario no menu
+			esc = t.nextInt();
+			t.nextLine();
+			processEsc(esc);
+		}
+		catch (InputMismatchException e) { //Se o usuario escolher algo invalido, vai ser tratado da seguinte maneira:
+			System.out.println("ERROR 003: //////Opção inválida. Por favor, insira um número válido.//////");
+			t.nextLine(); // Limpa o buffer após o erro
+		}
+		
+	}
+	System.out.println("Encerrando o programa.");
+	t.close(); //Retornara ao inicio do while
+}
+	public static void menu() {
+
 		System.out.println("Seja bem-vindo à nossa concessionária.\nFaça sua escolha:\n"
 				+ "1) Listar carros cadastrados.\n"
 				+ "2) Cadastrar novo carro.\n"
@@ -21,19 +42,37 @@ public class comercio {
 				+ "4) Remover um produto do comércio.\n"
 				+ "5) Vender algum produto existente.\n"
 				+ "6) Sair");
-		Scanner t = new Scanner(System.in);
-		int esc = Integer.parseInt(t.nextLine());
-		if (esc == 1) {
-			listar();
+		
+	}
+	
+
+	//!!!O tratamento de erros do menu so ocorre se existir esse metodo abaixo!!!
+	private void processEsc(int esc) //metodo para tratar as opcoes escolhidas no menu
+	{
+		switch (esc) {
+            case 1:
+                listar();
+                break;
+            case 2:
+				cadastrar();
+                break;
+            case 3:
+				adicionar();
+                break;
+            case 4:
+				remover();
+                break;
+			case 5:
+				vender();
+				break;
+            case 6:
+                System.out.println("Você escolheu sair.\nSaindo...");
+                break;
+            default:
+                System.out.println("Opção inválida. Por favor, escolha uma opção de 1 a 5.");
 		}
-		if (esc == 2) {
-			cadastrar();
-		}
 	}
-	catch(InputMismatchException e){
-		System.out.println("ERROR002: -----------A escolha da opcao do menu deve ser numerica (ex.: 1, 2, 3, etc.).-----------");
-	}
-	}
+
 	public boolean codigoExiste(long cod) {
 		item selecionado = new item(null);
 		for (item item : produtos.getProdutos()) {
@@ -62,4 +101,8 @@ public class comercio {
 	public void cadastrar() {
 		
 	}
+	public void vender() {
+
+	}
 }
+
